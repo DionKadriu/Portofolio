6d64cc825c7650ba7e7fcf5e8db8dc37d9148695
Dion Kadriu <dionkadriu@Dions-MacBook-Pro.local>
2025-09-27 00:47:18 +0100

    Changes the root file

--- pages/test.js
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-    return (
-      <div className="h-120 bg-blue-500 text-white flex items-center justify-center">
-        Tailwind Works 🎉
-      </div>
-    );
-  }
